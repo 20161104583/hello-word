ad45e6b756b8a1ee2a42faf326207f06095158bd
20161104583 <1657738522@qq.com>
2017-06-12 08:33:56 +0800

    hello word
    c

---
+++ hello word/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hello word
+//
+//  Created by 20161104583 on 17/6/12.
+//  Copyright © 2017年 20161104583. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
